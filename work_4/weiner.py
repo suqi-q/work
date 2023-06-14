dd83d7f7b7275cff871ff0977023f79eee33248e
Your Name <you@example.com>
2023-06-14 18:43:15 +0800

    作业

---
+++ work_4/weiner.py
@@ -0,0 +1,99 @@
+﻿import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False
+
+def generate_noise(img, sigma):
+    noise = np.random.randn(*img.shape) * sigma
+    noisy_img = np.clip(img + noise, 0, 255).astype(np.uint8)
+    return noisy_img, noise
+
+def weiner_filter(img, noise, snr):
+    N = np.fft.fft2(noise)
+    N_abs = np.abs(N) ** 2
+    Pn = np.sum(N_abs) / (N.shape[0] * N.shape[1])
+
+    Rf = np.fft.ifft2(np.abs(np.fft.fft2(img)) ** 2)
+    Rn = np.fft.ifft2(N_abs)
+
+    H = (np.conj(np.fft.fft2(img)) / (np.abs(np.fft.fft2(img)) ** 2 + snr * Pn)) * np.fft.fft2(img)
+    F = np.fft.ifft2(H * np.fft.fft2(noise))
+    restored_img = img + np.real(F)
+
+    return restored_img
+
+def mmse_filter(img, noise, snr):
+    N = np.fft.fft2(noise)
+    N_abs = np.abs(N) ** 2
+    Pn = np.sum(N_abs) / (N.shape[0] * N.shape[1])
+
+    Rf = np.fft.ifft2(np.abs(np.fft.fft2(img)) ** 2)
+    Rn = np.fft.ifft2(N_abs)
+
+    H = ((np.abs(np.fft.fft2(img)) ** 2) / (np.abs(np.fft.fft2(img)) ** 2 + snr * Pn)) * np.conj(np.fft.fft2(img)) / (np.abs(np.fft.fft2(img)) ** 2 + snr * Pn + Pn / snr)
+    F = np.fft.ifft2(H * np.fft.fft2(noise))
+    restored_img = img + np.real(F)
+
+    return restored_img
+
+def optimal_filter(img, noise):
+    N = np.fft.fft2(noise)
+    N_abs = np.abs(N) ** 2
+    Pn = np.sum(N_abs) / (N.shape[0] * N.shape[1])
+
+    Rf = np.fft.ifft2(np.abs(np.fft.fft2(img)) ** 2)
+    Rn = np.fft.ifft2(N_abs)
+
+    H = np.conj(np.fft.fft2(img)) / (np.abs(np.fft.fft2(img)) ** 2 + Pn / np.abs(np.fft.fft2(img)) ** 2) * np.fft.fft2(img)
+    F = np.fft.ifft2(H * np.fft.fft2(noise))
+    restored_img = img + np.real(F)
+
+    return restored_img
+
+img = cv2.imread('work_4\Lena.png', 0)
+
+sigma = 30
+noisy_img, noise = generate_noise(img, sigma)
+
+plt.subplot(121),plt.imshow(img,cmap='gray')
+plt.title('原图'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(noisy_img,cmap='gray')
+plt.title('噪声图像'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+restored_img_weiner_unknown = weiner_filter(img, noise, 0.1)
+restored_img_mmse_unknown = mmse_filter(img, noise, 0.1)
+restored_img_optimal_unknown = optimal_filter(img, noise)
+
+plt.subplot(131),plt.imshow(restored_img_weiner_unknown,cmap='gray')
+plt.title('Restored (Weiner, 信噪比未知)',fontsize=8), plt.xticks([]), plt.yticks([])
+plt.subplot(132),plt.imshow(restored_img_mmse_unknown,cmap='gray')
+plt.title('Restored (MMSE, 信噪比未知)',fontsize=8), plt.xticks([]), plt.yticks([])
+plt.subplot(133),plt.imshow(restored_img_optimal_unknown,cmap='gray')
+plt.title('Restored (最优的)',fontsize=8), plt.xticks([]), plt.yticks([])
+plt.show()
+
+snr = sigma**2 / np.var(noise)
+restored_img_weiner_known = weiner_filter(img, noise, snr)
+restored_img_mmse_known = mmse_filter(img, noise, snr)
+
+plt.subplot(121),plt.imshow(restored_img_weiner_known,cmap='gray')
+plt.title('Restored (Weiner, 信噪比已知)'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(restored_img_mmse_known,cmap='gray')
+plt.title('Restored (MMSE,信噪比已知)'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+Rf = np.fft.ifft2(np.abs(np.fft.fft2(img)) ** 2)
+Rn = np.fft.ifft2(np.abs(np.fft.fft2(noise)) ** 2)
+snr_map = np.abs(Rf) / np.abs(Rn)
+snr_map /= np.max(snr_map)
+
+restored_img_weiner_known_acf = weiner_filter(img, noise, snr_map)
+restored_img_mmse_known_acf = mmse_filter(img, noise, snr_map)
+
+plt.subplot(121),plt.imshow(restored_img_weiner_known_acf,cmap='gray')
+plt.title('Restored (Weiner, 自函数已知)'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(restored_img_mmse_known_acf,cmap='gray')
+plt.title('Restored (MMSE, 自函数已知)'), plt.xticks([]), plt.yticks([])
+plt.show()
---
+++ work_5/DPCM.py
@@ -0,0 +1,85 @@
+﻿import cv2
+import numpy as np
+from scipy import signal
+from skimage import metrics
+import matplotlib.pyplot as plt
+from skimage.metrics import peak_signal_noise_ratio as compare_psnr
+from skimage.metrics import structural_similarity as compare_ssim
+
+img = cv2.imread("work_5\Lena.png", 0)
+def dpcm_encode(img, qstep):
+
+    rows, cols = img.shape
+    pred_img = np.zeros((rows, cols))
+    err_img = np.zeros((rows, cols))
+
+
+    pred_img[0, :] = img[0, :]
+    pred_img[:, 0] = img[:, 0]
+
+    err_img[0, :] = img[0, :]
+    err_img[:, 0] = img[:, 0]
+
+    for i in range(1, rows):
+        for j in range(1, cols):
+            pred = (pred_img[i, j-1] + pred_img[i-1, j]) // 2
+            err = img[i, j] - pred
+            quant_err = np.round((err + 128) / qstep)
+            rec_err = quant_err * qstep - 128
+            pred_img[i, j] = pred
+            err_img[i, j] = rec_err
+    return err_img
+
+def dpcm_decode(dpcm_img, qstep):
+    rows, cols = dpcm_img.shape
+    pred_img = np.zeros((rows, cols))
+    rec_img = np.zeros((rows, cols))
+
+    pred_img[0, :] = dpcm_img[0, :]
+    pred_img[:, 0] = dpcm_img[:, 0]
+
+    rec_img[0, :] = dpcm_img[0, :]
+    rec_img[:, 0] = dpcm_img[:, 0]
+
+    for i in range(1, rows):
+        for j in range(1, cols):
+            pred = (pred_img[i, j-1] + pred_img[i-1, j]) // 2
+
+            rec_err = dpcm_img[i, j] * qstep
+
+            rec = pred + rec_err
+
+            pred_img[i, j] = pred
+            rec_img[i, j] = rec
+
+    return rec_img
+
+
+qsteps = [1, 2, 4, 8]
+num_steps = len(qsteps)
+
+plt.figure(figsize=(10, 8))
+rows = 2
+cols = 3
+plt.subplot(rows, cols, 1)
+plt.imshow(img, cmap='gray')
+plt.title('Original Image')
+
+for i, qstep in enumerate(qsteps):
+    dpcm_img = dpcm_encode(img, qstep)
+
+    rec_img = dpcm_decode(dpcm_img, qstep)
+
+    psnr_value = metrics.peak_signal_noise_ratio(img, rec_img, data_range=255)
+    ssim_value = metrics.structural_similarity(img, rec_img, win_size=3, data_range=255)
+
+    print('Quantization step: {}'.format(qstep))
+    print('PSNR: {} dB'.format(psnr_value))
+    print('SSIM: {}\n'.format(ssim_value))
+    plt.subplot(rows, cols, i+2)
+    plt.imshow(rec_img.astype(np.uint8), cmap='gray')
+    plt.title('QStep={}'.format(qstep))
+
+plt.tight_layout(pad=1.0, w_pad=1.0, h_pad=1.0)
+plt.show()
+
